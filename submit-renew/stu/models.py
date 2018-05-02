b4d947721576a1547abd2fad36bfef4031a0d6ec
charlie <charlielbj@163.com>
2018-05-02 16:23:30 +0800

    新增ticket切片，页面分页知识

---
+++ submit-renew/stu/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+
+    s_name = models.CharField(max_length=10, null=True)
+    s_tel = models.CharField(max_length=11, null=True)
+
+    class Meta:
+        db_table = 'sub_student'
+
+
+class StudentInfo(models.Model):
+
+    i_addr = models.CharField(max_length=30, null=True)
+    i_image = models.ImageField(upload_to='upload', null=True)
+    s = models.OneToOneField(Student)
+
+    class Meta:
+        db_table = 'sub_Student_Info'
